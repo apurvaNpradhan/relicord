90d5c214a12348166d9ff888e0597a39da03494d
apurvaNpradhan <apurvanarayanpradhan@gmail.com>
2026-03-24 01:24:24 +0530

    feat: removing alchemy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,4 @@
 import path from "node:path";
-
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
