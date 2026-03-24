eb506521f1201f9ce8b2fbfc1d17b6e3bac783a9
apurvaNpradhan <apurvanarayanpradhan@gmail.com>
2026-03-24 10:00:11 +0530

    refactor: bug in deployment

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -70,7 +70,7 @@ fastify.get("/", async () => {
 	return "OK";
 });
 
-fastify.listen({ port: 3000 }, (err) => {
+fastify.listen({ port: 3000, host: "0.0.0.0" }, (err) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
